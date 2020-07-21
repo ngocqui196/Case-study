9e7e120a4466e9f0e04c239e628c2bc95ad70765
ngocqui196 <58946367+ngocqui196@users.noreply.github.com>
2020-07-21 17:35:33 +0700

    sua lai

--- javascript.js
+++ javascript.js
@@ -1,21 +1,67 @@
-function myPlanes(image, hight, weight, size) {
-    this.image = image;
-    this.hight = hight;
-    this.weight = weight;
-    this.size = size;
-    this.getWith = function () {
-        return this.hight
+let img = document.getElementById("myCanvas").getContext("2d");
+let planes = {};
+let bullet = [];
+let count = 0;
+// Khởi tạo lớp máy bay của người chơi;
+function MyPlanes(toaDoX,toaDoY,width,height) {
+    this.image = new Image();
+    this.image.src = "planes.jpg";
+    this.toadoX = toaDoX;
+    this.toadoY = toaDoY;
+    this.width = width;
+    this.height = height;
+    this.getToaDoX = function () {
+        return this.toadoX;
     };
-    this.getHeight = function () {
-        return this.weight;
+    this.getToaDoY = function () {
+        return this.toadoY;
     };
     this.creatPlanes = function () {
-        return '<img width="'+ this.size + '"' +
-            ' height="'+ this.size + '"' +
-            ' src="' + this.image +'"' +
-            ' style="position:absolute;" />';
+        img.drawImage(this.image,this.toadoX,this.toadoY,this.width,this.height)
+    };
+
+}
+  //Khởi tạo lớp đạn:
+function Bullet (x,y,w,h) {
+    this.image = new Image();
+    this.image.src = "dan.png";
+    this.topY = 2;
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.creatBullet = function () {
+        this.y -= this.topY;
+        img.drawImage(this.image,this.x,this.y,this.w,this.h);
     }
 }
-let creatPlanes = document.getElementById("myCanvas").getContext("2d");
-let planes = new myPlanes('planes.jpg', 100, 100, 15);
-creatPlanes.drawImage(planes.creatPlanes(),0,0,planes.getWith(),planes.getHeight());
+
+window.addEventListener("mousemove", moveMouse);
+function drawAll() {
+    img.clearRect(0,0,535,380);
+    for (let i = 0; i < bullet.length; i++) {
+        bullet[i].creatBullet();
+    }
+    planes.creatPlanes();
+}
+// Bắn ra đạn:
+function creatNewBullet() {
+    count++;
+    bullet[count] = new Bullet(planes.getToaDoX() ,planes.getToaDoY(),29,13)
+}
+function startGame() {
+    planes = new MyPlanes(200,200,29,18);
+    let moveright = planes.getToaDoX() + 20;
+    bullet[count] = new Bullet(moveright,planes.getToaDoY(),25,18);
+    setInterval(drawAll,parseFloat("0.001"));
+    setInterval(creatNewBullet,100);
+
+
+}
+
+function moveMouse(evt) {
+    let canvas_x = evt.pageX -9;
+    let canvas_y = evt.pageY -9;
+    planes.toadoX = canvas_x -14;
+    planes.toadoY = canvas_y - 9;
+}
