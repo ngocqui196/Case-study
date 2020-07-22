279ec6c102099df7b1fc913aa62fa2b5f3165237
ngocqui196 <58946367+ngocqui196@users.noreply.github.com>
2020-07-22 16:47:10 +0700

    sua lai

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 let canvas =document.getElementById("myCanvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 500;
+canvas.height = 500;
 
 let img = document.getElementById("myCanvas").getContext("2d");
 let planes = {};
@@ -8,6 +8,7 @@ let enplanes = [];
 let bullet = [];
 let number = 0;
 let count = 0;
+let score = 0;
 // Khởi tạo lớp máy bay của người chơi;
 function MyPlanes(toaDoX,toaDoY,width,height) {
     this.image = new Image();
@@ -39,8 +40,24 @@ function Bullet (x,y,w,h) {
     this.creatBullet = function () {
         this.y -= this.topY;
         img.drawImage(this.image,this.x,this.y,this.w,this.h);
-    }
+    };
+    this.checkDiem =function (enplanet) {
+        let bullet_x = this.x + this.w / 2;
+        let bullet_y = this.y + this.h / 2;
+        let check = false;
+        if ( Math.abs(enplanet.getXCenter() - bullet_x) < (enplanet.getWidthEnemy() + this.w) / 2
+        &&  Math.abs(enplanet.getYCenter() - bullet_y) < enplanet.getHeightEnemy() + this.h / 2) {
+            this.y = -100;
+            enplanet.toadoy = 2000;
+            check = true;
+        }
+        if (check){
+            score++;
+        }
+    };
+
 }
+
 // Khởi tjao lớp máy bay địch
 function EnemyPlanes (toadox,toadoy,wEnemy,hEnemy) {
     this.image = new Image();
@@ -50,6 +67,24 @@ function EnemyPlanes (toadox,toadoy,wEnemy,hEnemy) {
     this.toadoy = toadoy;
     this.wEnemy = wEnemy;
     this.hEnemy = hEnemy;
+    this.getXCenter = function () {
+        return this.toadox+this.wEnemy/2;
+    };
+    this.getYCenter = function () {
+        return this.toadoy + this.hEnemy/2;
+    };
+    this.getXEnemy = function () {
+        return this.toadox;
+    };
+    this.getYEnemy = function () {
+        return this.toadoy;
+    };
+    this.getWidthEnemy = function () {
+        return this.wEnemy;
+    };
+    this.getHeightEnemy = function () {
+        return this.hEnemy;
+    };
     this.creatPlanesEnemy = function () {
         this.toadoy += this.dy;
         img.drawImage(this.image,this.toadox,this.toadoy,this.wEnemy,this.hEnemy);
@@ -61,10 +96,14 @@ function drawAll() {
     img.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < bullet.length; i++) {
         bullet[i].creatBullet();
+        for (let j = 0; j <enplanes.length ; j++) {
+            bullet[i].checkDiem(enplanes[j]);
+        }
     }
     planes.creatPlanes();
-    for (let i = 0; i <enplanes.length; i++) {
-        enplanes[i].creatPlanesEnemy();
+    for (let j = 0; j <enplanes.length; j++) {
+        enplanes[j].creatPlanesEnemy();
+        bullet[number].checkDiem(enplanes[j])
     }
 }
 // Tạo đối tượng máy bay địch;
@@ -79,13 +118,14 @@ function creatNewBullet() {
     number++;
     bullet[number] = new Bullet(planes.getToaDoX() ,planes.getToaDoY(),29,13)
 }
+
 function startGame() {
     planes = new MyPlanes(200,200,29,18);
     bullet[number] = new Bullet(planes.getToaDoX(),planes.getToaDoY(),25,18);
     let random = Math.floor(Math.random() * (canvas.width));
     enplanes[count] = new EnemyPlanes(random, -10,27,25);
     setInterval(drawAll,parseFloat("0.001"));
-    setInterval(creatNewBullet,100);
+    setInterval(creatNewBullet,300);
     setInterval(creatNewEnemy,1500);
 
 }
